5df737345acb89331151412196a7187b284cbecb
Shravan <shravan@strac.io>
2025-06-02 19:42:44 -0700

    complete trees 1

---
+++ validateBinarySearchTree.py
@@ -0,0 +1,22 @@
+# Time Complexity : O(n)    
+# Space Complexity : O(h)   
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def validate(node, low, high):
+            if not node:
+                return True
+            if not (low < node.val < high):
+                return False
+            return (validate(node.left, low, node.val) and
+                    validate(node.right, node.val, high))
+        return validate(root, float('-inf'), float('inf'))
+        
